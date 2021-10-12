ac95c50c9ea623e4751509a05ac6cb2d28843f73
nagi-sato <2040890h@gsuite.kobe-u.ac.jp>
2021-10-12 12:13:34 +0900

    change1-q1

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -8,11 +8,16 @@ function setup(){
   strokeWeight(25);
   strokeCap(SQUARE);
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
+  arc(75,100 , 100, 100, QUARTER_PI * -3, QUARTER_PI * -3 + PI );
   // BLANK[1]
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
-  const red = color(196, 0, 0);
-  const blue = color(14, 47, 146);
-  const green = color(22, 131, 46);
-  const gray = color(77);
+
+noFill();
+strokeWeight(10);
+fill(14, 47, 146);//青塗
+circle(250, 100, 50);
+    (196, 0, 0);//赤塗り
+  const green = color(22, 131, 46);//緑塗
+  const gray = color(77);//灰色塗り
 }
