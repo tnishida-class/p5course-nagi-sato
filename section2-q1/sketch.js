d3c232540a027fbf457b50273e5ccba3094b9ba0
nagi-sato <2040890h@gsuite.kobe-u.ac.jp>
2021-10-19 12:18:11 +0900

    change2-p1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+noFill();
+  for(let i = 0; i < 10; i++){//繰り返しアピール
+    if(i<5){
+        stroke(0,0,255);
+    }//真ん中青
+  else{
+        stroke(255,0,0);
+    }//外側赤
+ellipse(50,50,i*10+10);
+}//円
 }
