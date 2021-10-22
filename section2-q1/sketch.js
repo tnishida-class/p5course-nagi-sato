8e5f563550e8ead3f20700ab3400110bb46e7ed9
nagi-sato <2040890h@gsuite.kobe-u.ac.jp>
2021-10-22 13:17:11 +0900

    change2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,12 +2,12 @@
 function setup(){
 noFill();
   for(let i = 0; i < 10; i++){//繰り返しアピール
-    if(i<5){
-        stroke(0,0,255);
+    if(i < 5){
+        stroke(0, 0, 255);
     }//真ん中青
   else{
-        stroke(255,0,0);
+        stroke(255, 0, 0);
     }//外側赤
-ellipse(50,50,i*10+10);
+ellipse(50, 50, i * 10 + 10);
 }//円
 }
