229e3ffdbd22e89cc56b84073bbff8fb1ad701d9
nagi-sato <2040890h@gsuite.kobe-u.ac.jp>
2021-10-31 17:01:40 +0900

    recentchange3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,14 +4,14 @@ let cycle;
 
 function setup(){
   createCanvas(200, 200);
-  count = 0;
-  cycle = 50;
+  count = 50;
+  cycle = 50;//拡大範囲？
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1 ) % cycle;
+  count = (count + 1.5 ) % cycle;//この式の意味がわからない
   let size = count;
   ellipse(width / 2, height / 2, size);
-
+  if(keyIsDown(CONTROL)){count*=2}//Ctrl押すと早くなる
 }
