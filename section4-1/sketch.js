3d042e8a726a5f10b96edb92b776b1d5a0e4979d
nagi-sato <2040890h@gsuite.kobe-u.ac.jp>
2021-11-04 17:39:56 +0900

    change4-1

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -6,45 +6,51 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(20, 100); // scoresのiの場所に20以上100未満のランダムな数を代入
   }
-  console.log(scores);
+  console.log(scores);//index0~9　10個のデータがscore内に
 
   // 合計を計算する
   let sum = 0;
-  for(let i = 0; i < scores.length; i++){
-    sum += scores[i];
+  for(let i = 0; i < scores.length; i++){//iは0以上データ数未満
+    sum += scores[i];//index0~iまでデータ合計
   }
-  console.log(sum);
+  console.log(sum);//合計でました
 
   // ここから平均・最大・最小を求めます
   let average, largest, smallest;
-  // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
+  　average　=　sum　/　scores.length;// BLANK[1]　平均値＝合計÷データ数
+
 
   largest = 0;
-  for(let i = 0; i < scores.length; i++){
-    // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+  for(let i = 0; i < scores.length; i++){//iは0以上データ数(10)未満
+  　if (largest < scores[i]){largest = scores[i];}  // BLANK[2]　いままでの最大値と比較
   }
+
 
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
-    // BLANK[3]　ヒント：最小値とだいたい同じ
+    if (smallest > scores[i]){smallest = scores[i];}// BLANK[3]　いままでの最小値と比較
   }
 
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
-  
+  //iは0以上9以下（10本の横線）　始点(0, height*i/n)　終点(width, height*i/n)
+
   noStroke();
 
-  for(let i = 0; i < scores.length; i++){
-    const dx = width / scores.length;
-    const h = height * scores[i] / 100;
-    // BLANK[4] ヒント: 条件分岐を使って色を変更します
-    rect(i * dx + 2, height - h, dx - 4, h);
-    fill(0);
-    text(scores[i].toPrecision(3), i * dx, height - h);
-  }
+  for(let i = 0; i < scores.length; i++){//iは0以上9以下
+    const dx = width / scores.length;//定義　棒幅＝width/データ数
+    const h = height * scores[i] / 100;//定義　棒高さ＝height×データ数値÷100　
+    if (scores[i] == largest){fill(245, 29, 5);} //棒高さ95以上なら赤
+   else if (scores[i] == smallest){fill(5, 25, 245);}//棒高さ60以下なら青
+   else {fill(109, 111, 115);}//BLANK[4] それ以外なら灰色
+   rect(i * dx + 2, height - h, dx - 4, h);
+  fill(0);
+  text(scores[i].toPrecision(3), i * dx, height - h);}
 
+   stroke(0, 176, 3);
+   line(0, height-height * average / 100, width, height-height* average / 100);
   // BLANK[5] 平均点の線を引きます
 }
