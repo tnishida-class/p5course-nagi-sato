cc5519b1562ee99c4808cc3a76104836c34baccb
nagi-sato <2040890h@gsuite.kobe-u.ac.jp>
2021-11-12 09:23:10 +0900

    change5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -3,16 +3,20 @@
 // 吹き出しにしっぽを付けてみよう
 function setup(){
   createCanvas(400, 400);
-  background(255);
+  background(255);//背景　白
   balloon("I love keyakizaka46");
 }
 
 function balloon(t){
-  let w = textWidth(t);
+  let w = textWidth(t);//w＝テキストの長さ
   let h = textAscent() + textDescent();
+  //h＝最も上に飛び出してる上限+最も下に突き抜けてる下限
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  fill(0);//吹き出し色　黒
+   rect(0, 0, w + p * 2, h + p * 2);
+  //吹き出し　始点(0, 0)　幅テキスト長さ+4　高さh+4
+　triangle(w + p * 2, h + p * 2, w + p * 2, (h + p * 2)/ 2, (w + p * 2)+(h + p * 2) / 2, (h + p * 2)*3 / 4);
+  //吹き出し尻尾
+  fill(255);//文字色　白
+   text(t, p, h + p);//tを幅pに，高さh+2（吹き出し幅の半分）に
 }
